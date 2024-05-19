442ccb6db90df848b2006ac159e86469ecf54f9f
chutima puthachon <chutima.p8@gmail.com>
2024-05-19 21:17:24 +1000

    added intro.js

--- src/intro/Intro.js
+++ src/intro/Intro.js
@@ -3,9 +3,12 @@ import "../styles/intro.css";
 
 const Intro = () => {
   return (
-    <div id="intro">
-      <h1>CHUTIMA PUTHACHON</h1>
-      <h2>Software developer</h2>
+    <div className="intro-wrap">
+      <div id="intro">
+        {/* <i class="fa-solid fa-caret-right"></i> */}
+        <h1>CHUTIMA PUTHACHON</h1>
+        <h2>Software developer 🚀 </h2>
+      </div>
     </div>
   );
 };
