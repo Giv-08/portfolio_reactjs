9ae983d47147987c6ac05a914f8cd02142123baa
chutima puthachon <chutima.p8@gmail.com>
2024-05-19 20:58:31 +1000

    added intro.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./about/About.js";
 import Skills from "./skills/Skills.js";
 import Projects from "./projects/Projects.js";
 import Contact from "./contact/Contact.js";
+import Intro from "./intro/Intro.js";
 import "./styles/app.css"
 import Background from "./background/Background.js";
 
@@ -19,6 +20,7 @@ const App = () => {
       <Route path="/projects" element={<Projects/>}/>
       <Route path="/contatc" element={<Contact/>}/>
     </Routes>
+    <Intro />
   </Router>
   );
 };
---
+++ src/intro/Intro.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/intro.css";
+
+const Intro = () => {
+  return (
+    <div id="intro">
+      <h1>CHUTIMA PUTHACHON</h1>
+      <h2>Software developer</h2>
+    </div>
+  );
+};
+
+export default Intro;
