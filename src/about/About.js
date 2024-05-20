c2dcbd432967bc09e0b3af1eef30a082cea30781
chutima puthachon <chutima.p8@gmail.com>
2024-05-20 12:39:28 +1000

    added avatar css

--- src/about/About.js
+++ src/about/About.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-import Avatar from "../assets/avatar2.png";
-import AboutMenu from "./AboutMenu.js";
+import Avatar from "../avatar/Avatar";
+// import AboutMenu from "./AboutMenu.js";
 
 export default class About extends Component {
   render() {
     return (
     <>
       <Avatar page="about" />
-      <AboutMenu />
+      {/* <AboutMenu /> */}
     </>
   )
   }
---
+++ src/avatar/Avatar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+import avatarImage from "../assets/avatar4.png";
+import "../styles/avatar.css";
+
+const Avatar = ({ page }) => {
+  const avatarClass = `avatar ${page}`;
+  const spanClass = `shadow-overlay-${page}`;
+
+  return (
+    <>
+      <span className={spanClass}></span>
+      <img src={avatarImage} className={avatarClass} alt="avatar" />
+    </>
+  );
+};
+// to make sure the data you receive is valid
+Avatar.propTypes = {
+  page: PropTypes.string.isRequired,
+};
+
+export default Avatar;
