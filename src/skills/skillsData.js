bbebfde891cb42cadcc1018c574b6a0478f1e689
chutima puthachon <chutima.p8@gmail.com>
2024-05-21 10:07:09 +1000

    added skills

---
+++ src/skills/skillsData.js
@@ -0,0 +1,50 @@
+const skills = {
+  1: [
+    {
+      title: "HTML",
+    },
+    {
+      title: "CSS",
+    },
+    {
+      title: "SASS",
+    },
+    {
+      title: "JavaScript",
+    },
+    {
+      title: "Tailwind CSS",
+    },
+    {
+      title: "React JS",
+    },
+    {
+      title: "Figma",
+    },
+  ],
+  2: [
+    {
+      title: "PostgreSQL",
+    },
+    {
+      title: "SQL",
+    },
+    {
+      title: "Node.js",
+    },
+    {
+      title: "Git",
+    },
+    {
+      title: "Ruby",
+    },
+    {
+      title: "C",
+    },
+    {
+      title: "Python",
+    },
+  ],
+};
+
+export default skills;
